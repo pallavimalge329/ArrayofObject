0cccfa4a1e54b4beaa6082fcbc2eb9ca2d4beac3
HP <HP@DESKTOP-N5QBMVO>
2021-08-30 16:28:54 +0530

    Add project files.

---
+++ ArrayofObject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayofObject
+{
+    class Program
+    {
+        static int i;
+
+        static void Main(string[] args)
+        {
+            students[] s = new students[5];
+            //for (int i = 0; i < s.Lengts; i++)
+            //    s[i] = new students[5];
+            Console.WriteLine(s.Length);
+            for (i = 0; i < s.Length; i++)
+                s[i].getvals();
+
+            for (i = 0; i < s.Length; i++)
+                s[i].display();
+        }
+    }
+    class students
+    {
+        private int rno, marks;
+        private string sname;
+        public void getvals()
+        {
+            Console.WriteLine("enter roll no of student...");
+            rno = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter name of student...");
+            sname = Console.ReadLine();
+            Console.WriteLine("enter marks of student...");
+            marks = int.Parse(Console.ReadLine());
+        }
+        public void display()
+        {
+            Console.WriteLine("roll no = " + rno + "stu name = " + sname + "marks = " + marks);
+        }
+    }
+    }
+
