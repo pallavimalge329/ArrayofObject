a28a14d4bf0f86a3f2556aa2990a8400862f6be6
HP <HP@DESKTOP-N5QBMVO>
2021-08-30 16:53:31 +0530

    Bugs Resolved

--- ArrayofObject/Program.cs
+++ ArrayofObject/Program.cs
@@ -12,16 +12,22 @@ namespace ArrayofObject
 
         static void Main(string[] args)
         {
-            students[] s = new students[5];
+            students[] s = new students[2];
+            
             //for (int i = 0; i < s.Lengts; i++)
             //    s[i] = new students[5];
             Console.WriteLine(s.Length);
             for (i = 0; i < s.Length; i++)
+            {
+                s[i] = new students();
                 s[i].getvals();
+            }
 
             for (i = 0; i < s.Length; i++)
                 s[i].display();
+            Console.ReadKey();
         }
+
     }
     class students
     {
@@ -30,16 +36,17 @@ namespace ArrayofObject
         public void getvals()
         {
             Console.WriteLine("enter roll no of student...");
-            rno = int.Parse(Console.ReadLine());
+            rno = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter name of student...");
             sname = Console.ReadLine();
             Console.WriteLine("enter marks of student...");
-            marks = int.Parse(Console.ReadLine());
+            marks = Convert.ToInt32(Console.ReadLine());
         }
         public void display()
         {
             Console.WriteLine("roll no = " + rno + "stu name = " + sname + "marks = " + marks);
         }
     }
+
     }
 
